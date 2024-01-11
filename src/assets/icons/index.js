ed2c16e1ade2455157de4e1ce8418892f2aa11ac
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-01-11 23:39:35 +0100

    added assets, images, icons and index.js for the icons and images

---
+++ src/assets/icons/index.js
@@ -0,0 +1,24 @@
+import Icon from './Icon.svg'
+import Icon25 from './Icon25.svg'
+import Icon26 from './Icon26.svg'
+import client1 from './client1.svg';
+import client2 from './client2.svg';
+import client3 from './client3.svg';
+import client4 from './client4.svg';
+import client5 from './client5.svg';
+import client6 from './client6.svg';
+
+
+
+
+export {
+    Icon,
+    Icon25,
+    Icon26,
+    client1,
+    client2,
+    client3,
+    client4,
+    client5,
+    client6
+}
---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+import pana from './pana.svg'
+import illustration from './illustration.svg'
+import illustration2 from './illustration2.svg'
+import Logo from './Logo.png'
+
+
+export{
+    illustration,
+    illustration2,
+    pana,
+    Logo
+}
